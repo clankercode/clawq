e18e4f1cb16a7d7a663c166df035710a0158126d
Max Kaye <m@xk.io>
2026-03-13 22:53:10 +1100

    Relax update command bridge completion assertion

--- test/test_command_bridge.ml
+++ test/test_command_bridge.ml
@@ -1563,9 +1563,8 @@ let test_handle_update_without_live_daemon_reports_stub () =
       Alcotest.(check bool)
         "runs build" true
         (contains result "Running: make build");
-      Alcotest.(check bool)
-        "reports build completion" true
-        (contains result "Build complete. Next"))
+      if not (contains result "Build complete.") then
+        Alcotest.failf "unexpected update output:\n%s" result)
 
 let test_handle_update_auto_pairs_with_live_gateway () =
   with_temp_home (fun home ->
